29d9377f2b227fd4489e8fa10d523461e57edba2
Franklin-pro <Franklin-pro>
2024-03-07 08:35:16 +0200

    modules

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
-import router from './routes/index';
+import router from '../src/routes/index.js'
 import cors from 'cors'
 
 const app = express();
