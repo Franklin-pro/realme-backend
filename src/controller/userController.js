2d8f3dc715d36615af3557230fb71d669418577f
Franklin-pro <Franklin-pro>
2024-03-08 11:50:02 +0200

    test signup

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -9,7 +9,10 @@ import  jwt  from 'jsonwebtoken'
 class userCotroller{
 static async createUser(req,res){
 try {
-    const{firstName,lastName,email,password,confirmPassword,role}=req.body
+    const{firstName,lastName,email,password,role}=req.body
+    if(!password){
+        return errorMessage(res,401,`password is required`)
+    }
     if(req.body.password !== req.body.confirmPassword){
         return errorMessage(res,401,`password and confirmpassword must be matched`)
     }
@@ -28,7 +31,7 @@ static async getAllUser(req,res){
     const user = await User.find()
 
     if(!user || user==0){
-        return errorMessage(res,201,`no user found`)
+        return errorMessage(res,401,`no user found`)
     }else{
         return successMessage(res,200,`all users found ${user.length}`,user)
     }
@@ -37,16 +40,16 @@ static async deleteAllUser(req,res){
     const users = await User.deleteMany();
 
     if(!users){
-        return errorMessage(res,201,`no users deleted`)
+        return errorMessage(res,401,`no users deleted`)
     }else{
-        return errorMessage(res,201,`all users deleted`)
+        return errorMessage(res,200,`all users deleted`)
     }
 }
 static async getOneUser(req,res){
     const id = req.params.id
     const user = await User.findById(id)
     if(!user){
-        return errorMessage(res,201,`no user found ${id}`)
+        return errorMessage(res,401,`no user found ${id}`)
     }else{
         return successMessage(res,200,`user found`,user)
     }
@@ -55,16 +58,16 @@ static async deleteOneUser(req,res){
     const id = req.params.id
     const user = await User.findByIdAndDelete(id)
     if(!user){
-        return errorMessage(res,201,`no user delete by ${id}`)
+        return errorMessage(res,401,`no user delete by ${id}`)
     }else{
-        return errorMessage(res,201,`user delete`)
+        return errorMessage(res,200,`user delete`)
     }
 }
 static async updateUser(req,res){
     const id = req.params.id
     const user = await User.findByIdAndUpdate(id,req.body,{new:true})
     if(!user){
-        return errorMessage(res,201,`no user updated by ${id}`)
+        return errorMessage(res,401,`no user updated by ${id}`)
     }else{
         return successMessage(res,200,`user updated`,user)
     }
@@ -74,7 +77,7 @@ static async login(req,res){
 
     const user = await User.findOne({email})
     if(!user){
-        return errorMessage(res,201,`invalid email or password`)
+        return errorMessage(res,401,`invalid email or password`)
     }else{
         const comparePassword = bcrypt.compareSync(password,user.password)
         if(!comparePassword){
