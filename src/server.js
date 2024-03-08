1ed776ba02ca1ad916044e896c9e757c7fc258ec
Franklin-pro <Franklin-pro>
2024-03-08 11:23:33 +0200

    test

--- src/server.js
+++ src/server.js
@@ -1,34 +1,28 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import router from '../src/routes/index.js';
-import cors from 'cors';
+import express from 'express'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import router from '../src/routes/index.js'
+import cors from 'cors'
 
 const app = express();
 dotenv.config();
-app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.json())
+app.use("/",router)
 
-// Configure CORS with custom options
-app.use(cors({
-  origin: 'http://127.0.0.1:5500', 
-  methods: ['GET', 'POST'], 
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-}));
 
-app.use("/", router);
-
-const port = process.env.PORT;
-const database = process.env.DATABASE;
+const port = process.env.PORT
+const database = process.env.DATABASE
 
 // ------config port --------
 
-app.listen(port, () => {
-  console.log(`port running on ${port}`);
-});
+app.listen(port,()=>{
+    console.log(`port running on ${port}`)
+})
 
-mongoose.connect(database).then(() => {
-  console.log(`database connection successfully`);
-});
+mongoose.connect(database) .then(()=>{
+    console.log(`database connection successfully`)
+})
 
-export default app;
+export default app
