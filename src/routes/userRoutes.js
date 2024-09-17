0b2a93e17130f00dc2c159d4f8b9a434f0aa0ef2
Programmer-works <programmerfranklin@gmail.com>
2024-09-17 14:21:21 +0200

    good api

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,8 +7,8 @@ import verifyAccess from "../middleware/verifyAccess.js";
 const router = express.Router()
 
 router.post("/",dataChequer.inputempty,dataChequer.emailExist,validator.userAccountRule(),validator.inputvalidator,userCotroller.createUser)
-router.get("/",verifyAccess("admin"),userCotroller.getAllUser)
-router.delete("/",verifyAccess("admin"),userCotroller.deleteAllUser)
+router.get("/",userCotroller.getAllUser)
+router.delete("/",userCotroller.deleteAllUser)
 router.get("/:id",userCotroller.getOneUser)
 router.delete("/:id",userCotroller.deleteOneUser)
 router.patch("/:id",userCotroller.updateUser)
